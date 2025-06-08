8c6a79c15db17e149f9f274e9b7ef7b2148f6aa7
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2025-06-08 14:55:12 +0530

    feat: added basic consts

---
+++ src/constants.rs
@@ -0,0 +1,8 @@
+//! Contains all the constants required for ssz implementation.
+
+/// Number of bytes per chunk.
+pub const BYTES_PER_CHUNK: usize = 32;
+/// Number of bytes per serialized length offset.
+pub const BYTES_PER_LENGTH_OFFSET: usize = 4;
+/// Number of bits per byte.
+pub const BITS_PER_BYTE: usize = 8;
